04308f98bc0d75271410d01e700ae800cb1cbe6f
Miguel Barraza <Miguel@MacBook-Pro-de-Miguel.local>
2020-03-18 23:05:28 -0300

    class person

---
+++ Laboratorio_1/Laboratorio_1/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Laboratorio_1
+{
+    public class Human
+    {
+        public string name;
+        public string lastname;
+        public int RPS()
+        {
+            Random _throw = new Random();
+            return _throw.Next(0, 3);
+        }
+    }
+}
